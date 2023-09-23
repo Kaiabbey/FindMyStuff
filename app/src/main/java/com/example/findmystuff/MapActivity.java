3041366cfbb1bcf55a82387b7c71732d9f44170f
Matheus Kikumori Almeida Leite <matheuskiku@gmail.com>
2023-09-23 18:38:52 +1000

    IAB330_Matheus

---
+++ app/src/main/java/com/example/findmystuff/MapActivity.java
@@ -0,0 +1,21 @@
+package com.example.findmystuff;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.bottomsheet.BottomSheetBehavior;
+
+public class MapActivity extends AppCompatActivity {
+    private BottomSheetBehavior bottomSheetBehavior;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map);
+
+        View bottomSheet = findViewById(R.id.bottom_sheet_map);
+
+        bottomSheetBehavior = BottomSheetBehavior.from(bottomSheet);
+    }
+}
