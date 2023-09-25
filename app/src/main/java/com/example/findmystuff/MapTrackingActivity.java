528a8064f33c54f09064dd2c4e6892f1cbea1cfa
Matheus Kikumori Almeida Leite <74317831+mKikumori@users.noreply.github.com>
2023-09-25 15:17:35 +1000

    Update MapTrackingActivity.java

--- app/src/main/java/com/example/findmystuff/MapTrackingActivity.java
+++ app/src/main/java/com/example/findmystuff/MapTrackingActivity.java
@@ -1,21 +1,44 @@
 package com.example.findmystuff;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.google.android.material.bottomsheet.BottomSheetBehavior;
 
 public class MapTrackingActivity extends AppCompatActivity {
     private BottomSheetBehavior bottomSheetBehavior;
+    private View bottomSheet;
+    private ImageView callMessage;
+    private TextView callText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map_tracking);
 
-        View bottomSheet = findViewById(R.id.bottom_sheet_tracking);
+        setViewsIds();
 
         bottomSheetBehavior = BottomSheetBehavior.from(bottomSheet);
+
+        callMessage.setOnClickListener(view -> {
+            Intent intent = new Intent(MapTrackingActivity.this, MessagingActivity.class);
+            startActivity(intent);
+        });
+
+        callText.setOnClickListener(view -> {
+            Intent intent = new Intent(MapTrackingActivity.this, MessagingActivity.class);
+            startActivity(intent);
+        });
+    }
+
+    private void setViewsIds() {
+        bottomSheet = findViewById(R.id.bottom_sheet_tracking);
+        callMessage = findViewById(R.id.call_message);
+        callText = findViewById(R.id.call_text);
     }
 }
