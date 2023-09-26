aac3abb7aac23142e63e45d9608a7cd677373178
Matheus Kikumori Almeida Leite <74317831+mKikumori@users.noreply.github.com>
2023-09-26 13:41:02 +1000

    Update MessagingActivity.java

--- app/src/main/java/com/example/findmystuff/MessagingActivity.java
+++ app/src/main/java/com/example/findmystuff/MessagingActivity.java
@@ -1,12 +1,29 @@
 package com.example.findmystuff;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class MessagingActivity extends AppCompatActivity {
+    private ImageButton return_button;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_messaging);
+
+        setViewsIds();
+
+        return_button.setOnClickListener(view -> {
+            Intent intent = new Intent(this, MapTrackingActivity.class);
+            startActivity(intent);
+        });
+    }
+
+    private void setViewsIds() {
+        return_button = findViewById(R.id.returnButton);
     }
 }
