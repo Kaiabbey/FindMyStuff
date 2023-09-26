c9d49d65f0f2c4b20728cf57ac7ee402ce4aca4a
kai <kai.j.abbey@gmail.com>
2023-09-26 15:16:17 +1000

    changed button colour

--- app/src/main/java/com/example/findmystuff/CreateDeviceActivity.java
+++ app/src/main/java/com/example/findmystuff/CreateDeviceActivity.java
@@ -5,12 +5,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.RadioButton;
 import android.widget.Toast;
 
 public class CreateDeviceActivity extends AppCompatActivity {
     private Button backButton;
     private Button createDeviceButton;
     private Button testConnectionButton;
+    private RadioButton dongleRadio;
+    private RadioButton computerRadio;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,11 +33,21 @@ public class CreateDeviceActivity extends AppCompatActivity {
             Toast.makeText(CreateDeviceActivity.this, "Device Created!",Toast.LENGTH_SHORT).show();
             startActivity(intent);
         });
+
+        computerRadio.setOnClickListener(view ->{
+            Toast.makeText(CreateDeviceActivity.this, "COMPUTER!",Toast.LENGTH_LONG).show();
+        });
+
+        dongleRadio.setOnClickListener(view ->{
+            Toast.makeText(CreateDeviceActivity.this, "DONGLE!",Toast.LENGTH_LONG).show();
+        });
     }
 
     private void setViewIds(){
         backButton = findViewById(R.id.BackButton);
         createDeviceButton = findViewById(R.id.CreateDeviceButton);
         testConnectionButton = findViewById(R.id.TestConnectionButton);
+        computerRadio = findViewById(R.id.radio_computer);
+        dongleRadio = findViewById(R.id.radio_dongle);
     }
 }
