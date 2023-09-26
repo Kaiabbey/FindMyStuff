848ae493cd82a665cc9fa1fa0f2c5f82d033bf9a
Matheus Kikumori Almeida Leite <matheuskiku@gmail.com>
2023-09-26 15:34:16 +1000

    modified:   .idea/gradle.xml 	modified:   .idea/misc.xml 	modified:   app/src/main/AndroidManifest.xml 	modified:   app/src/main/java/com/example/findmystuff/MapActivity.java 	modified:   app/src/main/java/com/example/findmystuff/MapTrackingActivity.java 	modified:   app/src/main/java/com/example/findmystuff/MessagingActivity.java 	modified:   app/src/main/res/layout/activity_map.xml 	modified:   app/src/main/res/layout/activity_map_tracking.xml 	modified:   app/src/main/res/layout/activity_messaging.xml

--- app/src/main/java/com/example/findmystuff/MapTrackingActivity.java
+++ app/src/main/java/com/example/findmystuff/MapTrackingActivity.java
@@ -1,21 +1,52 @@
 package com.example.findmystuff;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.google.android.material.bottomsheet.BottomSheetBehavior;
 
 public class MapTrackingActivity extends AppCompatActivity {
     private BottomSheetBehavior bottomSheetBehavior;
+    private View bottomSheet;
+    private ImageView callMessage;
+    private TextView callText;
+    private ImageButton return_button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map_tracking);
 
-        View bottomSheet = findViewById(R.id.bottom_sheet_tracking);
+        setViewsIds();
 
         bottomSheetBehavior = BottomSheetBehavior.from(bottomSheet);
+
+        callMessage.setOnClickListener(view -> {
+            Intent intent = new Intent(MapTrackingActivity.this, MessagingActivity.class);
+            startActivity(intent);
+        });
+
+        callText.setOnClickListener(view -> {
+            Intent intent = new Intent(MapTrackingActivity.this, MessagingActivity.class);
+            startActivity(intent);
+        });
+
+        return_button.setOnClickListener(view -> {
+            Intent intent = new Intent(this, MapActivity.class);
+            startActivity(intent);
+        });
+    }
+
+    private void setViewsIds() {
+        bottomSheet = findViewById(R.id.bottom_sheet_tracking);
+        callMessage = findViewById(R.id.call_message);
+        callText = findViewById(R.id.call_text);
+        return_button = findViewById(R.id.returnButton);
     }
 }
--- app/src/main/java/com/example/findmystuff/MessagingActivity.java
+++ app/src/main/java/com/example/findmystuff/MessagingActivity.java
@@ -1,12 +1,34 @@
 package com.example.findmystuff;
 
+
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
 public class MessagingActivity extends AppCompatActivity {
+    private ImageButton return_button;
+    private ImageView emojiButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_messaging);
+
+        setViewsIds();
+
+        return_button.setOnClickListener(view -> {
+            Intent intent = new Intent(this, MapTrackingActivity.class);
+            startActivity(intent);
+        });
+
+    }
+
+    private void setViewsIds() {
+        return_button = findViewById(R.id.returnButton);
+        emojiButton = findViewById(R.id.emoji_button);
     }
 }
